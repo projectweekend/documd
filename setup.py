700093923a90abc60cb6bd5a8a987921e2b3d1c3
Brian Hines <brian@projectweekend.net>
2016-04-01 22:26:01 -0500

    License

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 
 setup(
     name='documd',
-    version='0.0.2',
+    version='0.0.3',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     description='Simple markdown documentation from docstrings',
     url='https://github.com/projectweekend/documd',
+    license='MIT',
     packages=['documd'],
     install_requires=[],
 )
