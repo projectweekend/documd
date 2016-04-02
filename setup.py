16c6e74c43fae14bb4782c34abf7b23042b6ccbc
Brian Hines <brian@projectweekend.net>
2016-04-01 22:17:17 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='documd',
-    version='0.0.1',
+    version='0.0.2',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     description='Simple markdown documentation from docstrings',
