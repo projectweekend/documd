aafc8d27bab0f89dbbc90c3e6dc36533f1e37c6b
Brian Hines <brian@projectweekend.net>
2016-04-02 18:59:17 -0500

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='documd',
-    version='0.0.3',
+    version='0.1.0',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     description='Simple markdown documentation from docstrings',
